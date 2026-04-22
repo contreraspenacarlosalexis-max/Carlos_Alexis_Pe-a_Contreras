f96391e301414dc5b700a89c6ff9ef147ce11347
contreraspenacarlosalexis-max <contreraspenacarlosalexis@gmail.com>
2026-04-21 18:43:14 -0600

    Add files via upload

---
+++ P04positivo_negativo.py
@@ -0,0 +1,12 @@
+#carlos alexis peña contraras
+# Pedir el número
+numero = float(input("Ingresa un número: "))
+
+# Condiciones usando AND, OR y NOT
+if numero > 0 and not (numero == 0):
+    print("El número es positivo")
+elif numero < 0 or not (numero > 0):
+    if numero < 0:
+        print("El número es negativo")
+    else:
+        print("El número es cero")
