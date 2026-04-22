77cf2f8bd34abdd3da54e00d04a576a819f42291
contreraspenacarlosalexis-max <contreraspenacarlosalexis@gmail.com>
2026-04-21 19:46:47 -0600

    Add files via upload

---
+++ P07uso_def.py
@@ -0,0 +1,13 @@
+#carlos alexis peña contreras
+# Definir la función
+def sumar(a, b):
+    return a + b
+
+# Pedir datos
+num1 = int(input("Ingresa un número: "))
+num2 = int(input("Ingresa otro número: "))
+
+# Llamar a la función
+resultado = sumar(num1, num2)
+
+print("La suma es:", resultado)
