383148193e683845a79bf4e1d71be868724f8a0c
contreraspenacarlosalexis-max <contreraspenacarlosalexis@gmail.com>
2026-04-21 19:49:17 -0600

    Add files via upload

---
+++ P08librerias.py
@@ -0,0 +1,15 @@
+#carlos alexis peña contreras
+# Importar librerías
+import math
+import random
+
+# Pedir un número
+numero = float(input("Ingresa un número: "))
+
+# Usar funciones de la librería math
+raiz = math.sqrt(numero)
+print("La raíz cuadrada es:", raiz)
+
+# Usar funciones de la librería random
+aleatorio = random.randint(1, 10)
+print("Número aleatorio entre 1 y 10:", aleatorio)
