e67948a489cdc39dfb6d7f93c742b58d6a1b083d
contreraspenacarlosalexis-max <contreraspenacarlosalexis@gmail.com>
2026-04-21 19:06:53 -0600

    Add files via upload

---
+++ P04numero_dentro_rango.py
@@ -0,0 +1,7 @@
+#carlos alexis peña contreras 
+numero = int(input("Ingresa un número: "))
+
+if numero >= 10 and numero <= 20:
+    print("Está dentro del rango")
+elif numero < 10 or numero > 20:
+    print("Está fuera del rango")
